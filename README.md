3b84fc1cd5e91535509692685cc33d3f4c6fbd4c
hollton <holltonliu@163.com>
2020-01-31 11:53:06 +0800

    1.0.3 支持文字

--- README.md
+++ README.md
@@ -3,7 +3,17 @@
 [![npm](https://img.shields.io/npm/v/image-pdf.svg)](https://www.npmjs.com/package/image-pdf) 
 [![LICENSE MIT](https://img.shields.io/npm/l/image-pdf.svg)](https://www.npmjs.com/package/image-pdf)
 
-image-pdf 多张图片生成PDF文件
+image-pdf 图片或文字信息转成PDF文件
+
+## 功能点 Feature
+* 支持图片 base64 方式
+* 支持图片 src 方式
+* 支持文字
+* 支持分页，避免图片内容被截断
+
+## 效果 Demo
+* [Demo 代码](https://github.com/hollton/image-pdf/blob/master/examples/index.html)
+* [生成的 PDF](https://pan.baidu.com/s/17CJ2IBCLCSLdmnxcEud9Tg)
 
 ## 使用 Usage
 
@@ -25,14 +35,15 @@ imagePdf(imagesData, 'title')
 
 ## 参数 Params
 * @param {Array} images: [{
-*  data 图片信息，可为 base64 信息，或 src 地址
+*  type 插入类型，image 图片类型，text 文字类型。默认 image
+*  data 插入内容，image 时可为 base64 信息，或 src 地址；text 时为文字内容
 *  width 图片宽，图片信息为 src 时可不传
 *  height 图片高
+*  options: { 文字配置信息
+*   FontSize: 20, 文字大小
+*   Font: 'times', 字体
+*   FontType: 'bold' 字体样式 bold加粗，italic斜体，bolditalic加粗斜体
+*  }
 * }],
 * @param {String} title 下载pdf文件的名称
 * @param {Object} options 配置信息，待完善
-
-## 功能点 Feature
-* 支持图片 base64 方式
-* 支持图片 src 方式
-* 支持分页，避免图片内容被截断
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 /**
- * imagePdf 多张图片生成PDF文件
+ * imagePdf 图片或文字信息转成PDF文件
  * @param {Array} images: [{
- *  data 图片信息，可为 base64 信息，或 src 地址
+ *  type 插入类型，image 图片类型，text 文字类型。默认 image
+ *  data 插入内容，image 时可为 base64 信息，或 src 地址；text 时为文字内容
  *  width 图片宽，图片信息为 src 时可不传
- *  height 图片高
+ *  height 图片高，
+ *  options: { 文字配置信息，非必须
+ *   FontSize: 20, 文字大小
+ *   Font: 'times', 字体
+ *   FontType: 'bold' 字体样式 bold加粗，italic斜体，bolditalic加粗斜体
+ *  }
  * }],
  * @param {String} title 下载pdf文件的名称
  * @param {Object} options 配置信息，待完善
  * 支持图片 base64 方式
  * 支持图片 src 方式
+ * 支持文字
  * 支持分页，避免图片内容被截断
  */
 import jsPDF from 'jspdf'
@@ -32,6 +39,51 @@ const pdfPage = {
 
 const pdf = new jsPDF('', 'pt', 'a4')
 
+// pdf插入内容的位置
+let pdfPostion = 0
+
+// 插入图片
+const addImage = img => {
+    const imgPage = {
+        width: pdfPage.width,
+        height: img.height / img.width * pdfPage.width
+    }
+    if (imgPage.height > pdfPage.height) { // 图片高于一页pdf高度时，将图片截断生成多页pdf
+        let _pdfPostion = pdfPostion
+        let imgHeight = imgPage.height
+        while (imgHeight > 0) {
+            pdf.addImage(img.data, 'PNG', padding.width, _pdfPostion, imgPage.width, imgPage.height)
+            _pdfPostion -= pdfPage.height;
+            imgHeight += _pdfPostion;
+            if (imgHeight > 0) {
+                pdfPostion = imgHeight
+                pdf.addPage();
+            }
+        }
+    } else { // 否则将图片插入到pdf中，若pdf剩余高度不足以插入整张图时，新增一页并将插入位置重置0
+        if (pdfPostion + imgPage.height > pdfPage.height) {
+            pdf.addPage()
+            pdfPostion = 0
+        }
+        pdf.addImage(img.data, 'PNG', padding.width, pdfPostion, imgPage.width, imgPage.height)
+        pdfPostion += imgPage.height
+    }
+}
+
+// 插入文字
+const addText = text => {
+    const {FontSize = 16} = text.options
+    if (pdfPostion + FontSize > pdfPage.height) {
+        pdf.addPage()
+        pdfPostion = 0
+    }
+    Object.keys(text.options).forEach(key => {
+        pdf[`set${key}`](text.options[key])
+    })
+    pdf.text(padding.width, pdfPostion + FontSize, text.data)
+    pdfPostion += FontSize
+}
+
 /**
  * base64图片数组生成pdf
  * @param {*} images base64图片数组
@@ -39,32 +91,16 @@ const pdf = new jsPDF('', 'pt', 'a4')
  * @param {*} options 配置信息
  */
 const savePdf = (images, title, options) => {
-    // pdf插入图片的位置
-    let pdfPostion = 0
     images.forEach(img => {
-        const imgPage = {
-            width: pdfPage.width,
-            height: img.height / img.width * pdfPage.width
-        }
-        if (imgPage.height > pdfPage.height) { // 图片高于一页pdf高度时，将图片截断生成多页pdf
-            let _pdfPostion = pdfPostion
-            let imgHeight = imgPage.height
-            while (imgHeight > 0) {
-                pdf.addImage(img.data, 'JPEG', padding.width, _pdfPostion, imgPage.width, imgPage.height)
-                _pdfPostion -= pdfPage.height;
-                imgHeight += _pdfPostion;
-                if (imgHeight > 0) {
-                    pdfPostion = imgHeight
-                    pdf.addPage();
-                }
-            }
-        } else { // 否则将图片插入到pdf中，若pdf剩余高度不足以插入整张图时，新增一页并将插入位置重置0
-            if (pdfPostion + imgPage.height > pdfPage.height) {
-                pdf.addPage()
-                pdfPostion = 0
-            }
-            pdf.addImage(img.data, 'PNG', padding.width, pdfPostion, imgPage.width, imgPage.height)
-            pdfPostion += imgPage.height
+        switch (img.type) {
+            case 'image':
+                addImage(img)
+                break
+            case 'text':
+                addText(img)
+                break
+            default:
+                break
         }
     })
     pdf.save(`${title}.pdf`)
@@ -96,19 +132,22 @@ const imagePdf = (images, title, options) => {
         if (index < images.length) {
             const img = images[index]
             let _img = {
+                type: img.type || 'image',
+                data: img.data || '',
                 width: img.width || 100,
                 height: img.height || 100,
-                data: img.data || ''
+                options: img.options || {}
             }
-            if(_img.data.indexOf('data:image/') === -1) {
+            if(_img.type === 'image' && _img.data.indexOf('data:image/') === -1) {
                 const newImg = new Image()
                 newImg.onload = () => {
-                    _img = {
-                        width: newImg.width,
-                        height: newImg.height,
-                        data: getBase64Image(newImg)
-                    }
+                    _img.data = getBase64Image(newImg)
+                    _img.width = newImg.width
+                    _img.height = newImg.height
                     newImages.push(_img)
+                    formatImages(++index)
+                }
+                newImg.onerror = () => {
                     formatImages(++index)
                 }
                 newImg.setAttribute('crossOrigin', 'anonymous')
